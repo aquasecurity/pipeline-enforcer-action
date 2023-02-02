c1e0f20bfbb52eeb32e2845ec27cf13361984dff
tamirkiviti13 <tamir@argon.io>
2023-02-02 17:55:43 +0200

    add verbose option

--- src/post.ts
+++ src/post.ts
@@ -2,12 +2,14 @@ import * as core from '@actions/core'
 import {getExecOutput} from '@actions/exec'
 import * as fs from 'fs'
 
-const executeTraceeEnd = async () => {
+const executeTraceeEnd = async (verbose: boolean = false) => {
   if (!fs.existsSync('./tracee')) {
     throw new Error('Tracee Commercial was not found')
   }
 
-  const result = await getExecOutput('./tracee ci end')
+  const traceeCommand = `./tracee ci end ${verbose ? '-v' : ''}`
+
+  const result = await getExecOutput(traceeCommand)
   if (result.exitCode != 0) {
     throw new Error(result.stdout + result.stderr)
   }
@@ -15,8 +17,9 @@ const executeTraceeEnd = async () => {
 
 async function run(): Promise<void> {
   try {
+    const verbose = core.getInput('verbose') === 'true'
     core.info('Ending Tracee Commercial run')
-    await executeTraceeEnd()
+    await executeTraceeEnd(verbose)
     core.debug('Tracee Commercial ended successfully')
   } catch (error) {
     if (error instanceof Error) {
